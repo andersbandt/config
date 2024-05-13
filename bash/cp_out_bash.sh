9e8b217f0391c30003423af7fc99ef3f3303b077
andersbandt <andersbandt@yahoo.com>
2024-05-12 19:13:34 -0500

    cp out script works on Windows now

--- bash/cp_out_bash.sh
+++ bash/cp_out_bash.sh
@@ -1,4 +1,43 @@
 
 
-cp -vi .bashrc ~/
-cp -vi .bashrc_user ~/
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    system="Linux"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # Mac OSX
+    system="max"
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+    # POSIX compatibility layer and Linux environment emulation for Windows
+    system="Windows"
+elif [[ "$OSTYPE" == "msys" ]]; then
+    system="msys"
+    # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+    # ...
+    system="freebsd"
+else
+    # Unknown.
+    system=0
+fi
+
+
+echo "using system $system"
+echo "using onedrive '$ONEDRIVE'"
+
+
+cp -vi .bashrc         ~/
+cp -vi .bashrc_color   ~/
+
+
+
+if [ $system = "Windows" ]
+then
+    cp -iv .bashrc_user           /cygdrive/c/Users/ander/OneDrive/
+elif [ $system = "Linux" ]
+then
+    cp -vi .bashrc_user           ~/
+
+fi
+
+
+
