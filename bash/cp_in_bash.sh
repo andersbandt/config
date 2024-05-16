5a4785afabd39785cfbed1947f30174c594eb618
andersbandt <andersbandt@yahoo.com>
2024-05-16 16:32:30 -0500

    fixed onedrive on windows

--- bash/cp_in_bash.sh
+++ bash/cp_in_bash.sh
@@ -1,17 +1,39 @@
 
 
-source ~/.bashrc
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    system="Linux"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # Mac OSX
+    system="max"
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+    # POSIX compatibility layer and Linux environment emulation for Windows
+    system="Windows"
+elif [[ "$OSTYPE" == "msys" ]]; then
+    system="msys"
+    # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+    # ...
+    system="freebsd"
+else
+    # Unknown.
+    system=0
+fi
 
 
-#system="Linux"
+source ~/OneDrive/.bashrc_user
+
+
 echo "using system $system"
+echo "using onedrive '$ONEDRIVE'"
+
 
 cp -iv ~/.bashrc .
 cp -iv ~/.bashrc_color .
 
 
-echo "using onedrive '$ONEDRIVE'"
-#cp -iv $ONEDRIVE/.bashrc_user .
+
+
 
 if [ $system = "Windows" ]
 then
