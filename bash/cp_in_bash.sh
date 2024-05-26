6a3202df0b3098b9fd1634d8e0ea32394e80d30d
Anders <andersbandt@yahoo.com>
2024-05-26 01:01:26 +0100

    updated cp in/out scripts to show differences and prompt user

--- bash/cp_in_bash.sh
+++ bash/cp_in_bash.sh
@@ -24,9 +24,6 @@ else
 fi
 
 
-source ~/OneDrive/.bashrc_user
-
-
 echo "using system $system"
 echo "using onedrive '$ONEDRIVE'"
 
@@ -48,3 +45,56 @@ fi
 
    
 
+# Function to compare and move files
+move_with_diff() {
+    local src_file=$1
+    local dest_file=$2
+
+    if [[ -e "$dest_file" ]]; then
+        echo -e "\n\nDifferences between $src_file and $dest_file:"
+        if diff --color "$dest_file" "$src_file"; then
+            echo "No differences found between $src_file and $dest_file. Skipping copy."
+        else
+            echo "Do you want to overwrite $dest_file with $src_file? (y/n)"
+            read -r answer
+            if [[ "$answer" == "y" ]]; then
+                cp -vi "$src_file" "$dest_file"
+            else
+                echo "Skipping $src_file"
+            fi
+        fi
+    else
+        cp -vi "$src_file" "$dest_file"
+    fi
+}
+
+
+
+# Specify the files to be moved
+src_files=("$HOME/.bashrc" "$HOME/bashrc_color")
+dest_dir="."
+
+# Perform the move operation with diff check
+for src_file in "${src_files[@]}"; do
+    dest_file="$dest_dir/$(basename "$src_file")"
+    move_with_diff "$src_file" "$dest_file"
+done
+
+
+
+# move .bashrc_user
+# location specific depending on OS
+src_file_name=(".bashrc_user")
+if [ $system = "Windows" ]
+then
+    src_dir="/cygdrive/c/Users/ander/OneDrive/"
+elif [ $system = "Linux" ]
+then
+    src_dir="$HOME"
+fi
+
+
+src_file="$dest_dir/$(basename "$src_file_name")"
+dest_file="$dest_dir/$(basename "$src_file_name")"
+
+move_with_diff "$src_file" "$dest_file"
