b38247436eea221b8efbdeb323aa2a358b4fe491
andersbandt <andersbandt@yahoo.com>
2024-07-02 22:46:19 -0500

    major refactoring into file split

--- bash/cp_in_bash.sh
+++ bash/cp_in_bash.sh
@@ -3,27 +3,6 @@
 echo "executing COPY to IN"
 echo "Files in Github will be overwritten by local computer files"
 
-
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    system="Linux"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    # Mac OSX
-    system="max"
-elif [[ "$OSTYPE" == "cygwin" ]]; then
-    # POSIX compatibility layer and Linux environment emulation for Windows
-    system="Windows"
-elif [[ "$OSTYPE" == "msys" ]]; then
-    system="msys"
-    # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
-elif [[ "$OSTYPE" == "freebsd"* ]]; then
-    # ...
-    system="freebsd"
-else
-    # Unknown.
-    system=0
-fi
-
-
 echo "using system $system"
 echo "using onedrive '$ONEDRIVE'"
 
@@ -34,10 +13,11 @@ move_with_diff() {
     local dest_file=$2
 
     if [[ -e "$dest_file" ]]; then
-        echo -e "\n\nDifferences between $src_file and $dest_file:"
+        echo -e "\n\nLooking for differences between $src_file and $dest_file:"
         if diff --color "$dest_file" "$src_file"; then
             echo "No differences found between $src_file and $dest_file. Skipping copy."
         else
+	    echo -e "\tdifferences in RED are being eliminated in GitHub repo"
             echo "Do you want to overwrite $dest_file with $src_file? (y/n)"
             read -r answer
             if [[ "$answer" == "y" ]]; then
@@ -54,8 +34,9 @@ move_with_diff() {
 
 
 # Specify the files to be moved
-src_files=("$HOME/.bashrc" "$HOME/.bashrc_color")
+src_files=("$HOME/.bashrc" "$HOME/.bash_user" "$HOME/.bash_alias" "$HOME/.bash_os" "$HOME/.bash_color" )
 dest_dir="."
+
 
 # Perform the move operation with diff check
 for src_file in "${src_files[@]}"; do
@@ -65,19 +46,19 @@ done
 
 
 
-# move .bashrc_user
+# move .bash_user
 # location specific depending on OS
-src_file_name=(".bashrc_user")
-if [ $system = "Windows" ]
-then
-    src_dir="/cygdrive/c/Users/ander/OneDrive/"
-elif [ $system = "Linux" ]
-then
-    src_dir="$HOME"
-fi
+#src_file_name=(".bash_user")
+#if [ $system = "Windows" ]
+#then
+#    src_dir="/cygdrive/c/Users/ander/OneDrive/"
+#elif [ $system = "Linux" ]
+#then
+#    src_dir="$HOME"
+#fi
 
 
-src_file="$src_dir/$(basename "$src_file_name")"
-dest_file="$dest_dir/$(basename "$src_file_name")"
-
-move_with_diff "$src_file" "$dest_file"
+#    src_dir="$HOME"
+#src_file="$src_dir/$(basename "$src_file_name")"
+#dest_file="$dest_dir/$(basename "$src_file_name")"
+#move_with_diff "$src_file" "$dest_file"
