1e3df455925cb3266a6840f834849972342462e3
Anders <andersbandt@yahoo.com>
2024-09-04 10:46:44 -0500

    fixed cp out script

--- emacs/cp_out_emacs.sh
+++ emacs/cp_out_emacs.sh
@@ -28,20 +28,21 @@ for src_file in "${src_files[@]}"; do
 done
 
 
-
 # Specify the files to be moved
-src_files=("/.emacs.d/*el")
+src_files=(".emacs.d/*el")
 dest_dir="$HOME/.emacs.d/"
 
 
-# perform the move for other files in .emacs.d
+# Perform the move operation with diff check
 for src_file in "${src_files[@]}"; do
-    dest_file="$dest_dir/$(basename "$src_file")"
-    move_with_diff "$src_file" "$dest_file"
+    for file in $src_file; do
+        if [[ -e "$file" ]]; then  # Check if the file exists
+            dest_file="$dest_dir/$(basename "$file")"
+            move_with_diff "$file" "$dest_file"
+        else
+            echo -e "${YELLOW}No files found matching $src_file${ENDC}"
+        fi
+    done
 done
 
 
-
-# old, simple but functional script
-#\cp -vi .emacs ~/
-#\cp -vrf .emacs.d ~/
